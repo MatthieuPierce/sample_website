0abe1a901d91577077cd0116c15d26a0082b88d5
Matthieu Pierce <matthieu.pierce@gmail.com>
2020-06-17 17:25:53 -0700

    syncing gallery image and aside description

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,25 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let mainImage = document.querySelector("#gallery-photo img");
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      //set clicked image as main gallery image
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      let newAlt = thumbnail.alt;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", newAlt);
+
+      //Change which image is current
+      let currentClass = "current"
+      document.querySelector(currentClass).classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      //Update image info
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    })
+  })
 }
